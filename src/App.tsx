d5c4f01ad4ba32b83fb9b7a6ba40e4762c545828
XieZongChen <952937570@qq.com>
2025-03-04 09:46:59 +0800

    feat: 新建 OscillatorNode 节点

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,22 @@ import {
   useNodesState,
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
+import { OscillatorNode } from './components/OscillatorNode';
 
 const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  {
+    id: '1',
+    position: { x: 0, y: 0 },
+    data: { frequency: 300, type: 'square' },
+    type: 'osc',
+  },
+  { id: '2', position: { x: 0, y: 300 }, data: { label: '2' } },
 ];
 const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+const nodeTypes = {
+  osc: OscillatorNode,
+};
 
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
@@ -34,6 +44,7 @@ export default function App() {
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        nodeTypes={nodeTypes}
         fitView
       >
         <Controls />
