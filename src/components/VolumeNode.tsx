d84dc036e9a18cc932246885027fb6d8a6f501bf
XieZongChen <952937570@qq.com>
2025-03-05 09:57:28 +0800

    feat: VolumeNode 接入 updateAudioNode 逻辑

--- src/components/VolumeNode.tsx
+++ src/components/VolumeNode.tsx
@@ -1,4 +1,6 @@
 import { Handle, Position } from '@xyflow/react';
+import { ChangeEvent, ChangeEventHandler, useState } from 'react';
+import { updateAudioNode } from '../audio';
 
 export interface VolumeNodeProps {
   id: string;
@@ -8,6 +10,13 @@ export interface VolumeNodeProps {
 }
 
 export function VolumeNode({ id, data }: VolumeNodeProps) {
+  const [gain, setGain] = useState(data.gain);
+
+  const changeGain: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setGain(+e.target.value);
+    updateAudioNode(id, { gain: +e.target.value });
+  };
+
   return (
     <div className={'rounded-md bg-white shadow-xl'}>
       <Handle type='target' position={Position.Top} />
@@ -15,8 +24,16 @@ export function VolumeNode({ id, data }: VolumeNodeProps) {
       <p className={'rounded-t-md p-[4px] bg-blue-500 text-white'}>音量节点</p>
       <div className={'flex flex-col p-[4px]'}>
         <p>Gain</p>
-        <input type='range' min='0' max='1' step='0.01' value={data.gain} />
-        <p className={'text-right'}>{data.gain.toFixed(2)}</p>
+        <input
+          className='nodrag'
+          type='range'
+          min='0'
+          max='1'
+          step='0.01'
+          value={gain}
+          onChange={changeGain}
+        />
+        <p className={'text-right'}>{gain.toFixed(2)}</p>
       </div>
 
       <Handle type='source' position={Position.Bottom} />
