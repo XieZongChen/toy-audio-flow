369626bd08d65ae9a2b966a972f39b957369684c
XieZongChen <952937570@qq.com>
2025-03-05 09:33:30 +0800

    feat: 测试两个振荡器 connect 到一个 destination

--- src/audio.ts
+++ src/audio.ts
@@ -12,3 +12,14 @@ const out = context.destination;
 
 osc.connect(volume);
 volume.connect(out);
+
+const osc2 = context.createOscillator();
+osc2.frequency.value = 800;
+osc2.type = 'sine';
+osc2.start();
+
+const volume2 = context.createGain();
+volume2.gain.value = 0.5;
+
+osc2.connect(volume2);
+volume2.connect(out);
